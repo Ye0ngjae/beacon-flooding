c36d961e25ab617d78e24fecc6ccffc6a670134e
Ye0ngJae <22sunrin031@sunrint.hs.kr>
2023-07-14 12:36:40 +0900

    update

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def flood_beacon(ssid):
     dot11 = Dot11(type=0, subtype=8, addr1='ff:ff:ff:ff:ff:ff', addr2=RandMAC(), addr3=RandMAC())
     essid = Dot11Elt(ID='SSID', info=ssid, len=len(ssid))
     frame = RadioTap()/dot11/Dot11Beacon()/essid
-    sendp(frame, inter=0.1, loop=1, iface='Wi-Fi')  # wlan0mon은 무선 인터페이스 이름에 맞게 변경해야 합니다.
+    sendp(frame, inter=0.1, loop=1, iface='wlan0mon')  # wlan0mon은 무선 인터페이스 이름에 맞게 변경해야 합니다.
 
 @app.route('/stop_flooding', methods=['POST'])
 def stop_flooding():
